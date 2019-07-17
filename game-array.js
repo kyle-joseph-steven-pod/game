035f60b3df43aff57a3287a0dc846f0d021f246c
Kyle Maulsby <kylemaulsby15@gmail.com>
2019-07-17 12:35:24 -0500

    baseline html/css/js and array file

---
+++ game-array.js
@@ -0,0 +1,23 @@
+var characters = [
+    {
+        name: "Agent Cereas",
+        poses: {
+            idle_1: "Pixel-Art/Agent-Cereas.png",
+            idle_2: "",
+            left: "",
+            right: "",
+            attack: "",
+            damaged: "",
+            death: ""
+        },
+        base_stats:{
+            hp: 16,
+            strength: -1,
+            agility: 0,
+            sharp: 2,
+            presence: 1,
+            misses: 0,
+            // damageDie: d4damage()
+        }
+    }
+];
