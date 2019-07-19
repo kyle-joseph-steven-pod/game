694759a1ae1d0cf991677f581deedfe03e565a00
Kyle Maulsby <kylemaulsby15@gmail.com>
2019-07-19 09:02:14 -0500

    button update attempts

--- pod-game.js
+++ pod-game.js
@@ -1,4 +1,5 @@
 "use strict";
+let count;
 //TODO: logic to append text into the console output and have it show up below
 
 // $("#log-form").submit(function(e){
@@ -11,22 +12,23 @@
 //Adds text to output log, reuse as possible for dialogue
 
 // TODO Fight and Random talk quotes to be used elsewhere
-$("#button-fight").on("click",function(){
-    var dmg = characters[0].base_stats.damageDie();
-    var pText = `<p class='output-p'><span class='output-arrow'>-></span>Your attack hit, and did ${dmg} damage</p>`;
-    $("#log-output").append(pText).scrollTop(9999999999999999);
-});
+
+//  the function name pretty much explains itself here...//
+//  we can use this same code to make pretty much anything flash at a set interval - maybe something to keep in mind for the Character Damage Animation?
+
 $('#button-talk').on('click', function () {
     var randomPhrase = charSays[Math.floor(Math.random()*charSays.length)];
     var pText = "<p class='output-p'><span class='output-arrow'>-></span>" + randomPhrase + "</p>";
     $("#log-output").append(pText).scrollTop(9999999999999999);
 });
-
-//  the function name pretty much explains itself here...//
-//  we can use this same code to make pretty much anything flash at a set interval - maybe something to keep in mind for the Character Damage Animation?
+$("#button-fight").on("click",function(){
+    var dmg = characters[userChar].base_stats.damageDie();
+    var pText = `<p class='output-p'><span class='output-arrow'>-></span>Your attack hit, and did ${dmg} damage</p>`;
+    $("#log-output").append(pText).scrollTop(9999999999999999);
+});
 
 function flash() {
-    let count = 0;
+    count = 0;
     let elem = $("#startScreen");
     let flasher = setInterval(function () {
         elem.toggleClass("flash")
@@ -34,27 +36,40 @@ function flash() {
 
     //  First bit of interactivity for the user to pick their class and name and have the start screen
     //  transition to the written name
-    $("#button-talk").on("click",function() {
+    $("#button-left").on("click",function() {
         if(count === 0){
             count = 4;
         }
         count--;
         $('#con-sprite').css('background-image', 'url(' + characters[count].poses.right[0] + ')');
     });
-    $("#button-run").on("click",function() {
+    $("#button-right").on("click",function() {
         if(count === 3){
             count = -1;
         }
         count++;
         $('#con-sprite').css('background-image', 'url(' + characters[count].poses.right[0] + ')');
     });
-    $("#button-fight").on("click",function(){
+    $("#button-select").on("click",function(){
         elem.html("Choose Your Name");
         $("#log-form").submit(function(e){
+            let left =  $("#button-left");
+            let select =  $("#button-select");
+            let right =  $("#button-right");
+            const userChar = count;
             e.preventDefault();
             elem.html($("#log-form-input").val());
             clearInterval(flasher);
+            left.html("Talk");
+            select.html("Fight");
+            right.html("Run");
+            $(document).on('change', function () {
+               left.attr("id", "button-talk");
+               select.attr("id", "button-fight");
+               right.attr("id", "button-run");
+            });
             elem.removeClass("flash");
+
         });
     });
 }
