1b8e872e0d270dcd26216c306f948f4cc8ddd1a9
JustinSOrtega <justinsortega@gmail.com>
2019-07-24 15:35:36 -0500

    Syntax ex part 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,11 @@ public class HelloWorld {
 public static void main(String[] args) {
     System.out.print("Hello, World!");
     System.out.print("\n");
-    }
+
+//
+    int myFavoriteNumber = 42;
+    System.out.print(myFavoriteNumber);
+
+
+}
 }
