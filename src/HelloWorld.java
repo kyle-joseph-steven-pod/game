8090a5bfc6e21f373efb4b19cb09ad18a7f57ae8
JustinSOrtega <justinsortega@gmail.com>
2019-07-24 15:40:57 -0500

    Syntax ex part 5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,27 @@
 public class HelloWorld {
-public static void main(String[] args) {
-    System.out.print("Hello, World!");
-    System.out.print("\n");
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        System.out.print("\n");
 
 //
-    int myFavoriteNumber = 42;
-    System.out.println(myFavoriteNumber);
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
 //
-    String myString = "Chocolate Devotion is the best ice cream at Coldstone";
-    System.out.println(myString);
+        String myString = "Chocolate Devotion is the best ice cream at Coldstone";
+        System.out.println(myString);
+//
+        myString = "dogs";
+        System.out.println(myString);
+//
+//        myString = 3.14159;
+//        System.out.println(myString);
+//
+        long myNumber;
+//        System.out.println(myNumber);
+//        myNumber = 3.14;
+        myNumber = 123L;
+        System.out.println(myNumber);
 
 
-}
+    }
 }
