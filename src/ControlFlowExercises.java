6791903ff49a72aa06db76993a700e5c4a933bf4
JustinSOrtega <justinsortega@gmail.com>
2019-07-25 15:27:16 -0500

    issues with 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,5 +5,17 @@ public class ControlFlowExercises {
             System.out.println(i);
             ++i;
         }
+        //2
+        int a = 100;
+        do {
+            System.out.println(a);
+            a -= 5;
+        }while (a >= -10);
+
+        int b = 2;
+        do {
+            System.out.println(b);
+            b = b * b;
+        } while (b < 100000);
     }
 }
