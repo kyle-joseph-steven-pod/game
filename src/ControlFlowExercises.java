996d3b5e7b679fa3e234d7e3752060b47cc19087
JustinSOrtega <justinsortega@gmail.com>
2019-07-25 16:14:04 -0500

    finished loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
         int i = 5;
         while (i <= 15) {
             System.out.println(i);
@@ -12,29 +18,79 @@ public class ControlFlowExercises {
             a -= 5;
         } while (a >= -10);
 
-        int b = 2;
+        double b = 2;
         do {
             System.out.println(b);
-            b = b * b;
-        } while (b < 1000000);
+            b = Math.pow(b, 2);
+        } while (b <= 1000000);
+
+
         //For
         for (int c = 1; c >= -10; c++) {
             System.out.println(c);
             c -= 5;
         }
-        for (int d = 1; d <= 1000000; d*=d) {
+        for (double d = 2; d <= 1000000; d = Math.pow(d, 2)) {
             System.out.println(d);
         }
 
 //        2
-        for (int f = 1; f <= 50; f++) {
+        for (long f = 1; f <= 50; f += 1) {
             if (f % 15 == 0) {
                 System.out.println("Fizzbuzz");
-            } else if (f == 5) {
+            } else if (f % 5 == 0) {
                 System.out.println("Buzz");
-            } else if (f == 3) {
+            } else if (f % 3 == 0) {
                 System.out.println("Fizz");
-            } else System.out.println(i);
+            } else System.out.println(f);
         }
+//      3
+        System.out.println("What number would you like to go up to? ");
+        long userInput = scanner.nextInt()+1;
+        System.out.println("Here is your table!");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for (long g = 1; g <= userInput; g += 1) {
+            if (g == userInput) {
+                break;
+            } else {
+                System.out.println(g + "     | "+ (Math.pow(g, 2)) +"------ | "+ (Math.pow(g, 3)) +"----");
+            }
+
+        }
+        System.out.println("Would you like to continue? [y/n]");
+        String answer = scanner.next();
+        if (answer.equals("y")) {
+            long newNum = userInput + 5;
+            for (long g = userInput; g <= newNum+1; g += 1) {
+                if (g == newNum) {
+                    break;
+                } else {
+                    System.out.println(g + "     | "+ (Math.pow(g, 2)) +"------ | "+ (Math.pow(g, 3)) +"----");
+                }
+
+            }
+
+        } else {
+            System.out.println("Cya!");
+
+        }
+
+//        4
+        System.out.println("Lets convert some grades! Enter in a number grade: ");
+        int grade = scanner.nextInt();
+        if (grade >= 88) {
+            System.out.println("That's an A!");
+        } else if (grade >=80) {
+            System.out.println("That's a B!");
+        } else if (grade >=67) {
+            System.out.println("That's a C!");
+        } else if (grade >=60) {
+            System.out.println("That's a D!");
+        } else {
+            System.out.println("That's an F!");
+        }
+
+
     }
 }
