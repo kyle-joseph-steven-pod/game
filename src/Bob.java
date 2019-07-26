7de2aa2f1ba08e2f3afb69a60e2966e652f77e0c
JustinSOrtega <justinsortega@gmail.com>
2019-07-26 14:18:26 -0500

    EoD Friday

--- src/Bob.java
+++ src/Bob.java
@@ -5,10 +5,11 @@ public class Bob {
         System.out.println("Ask Bob the lackadaisical teen a question: ");
         Scanner scanner = new Scanner(System.in);
         String question = scanner.nextLine();
+
         if (question.endsWith("?")) {
             System.out.println("Sure");
         } else if (question.endsWith("!")) {
-            System.out.println("Sure");
+            System.out.println("Whoa, chill out!");
         } else  if (question.equals("")) {
             System.out.println("Fine. Be that way!");
         } else {
