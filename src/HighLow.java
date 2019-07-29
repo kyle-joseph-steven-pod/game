e97e86c9686e49b25845e69135640d391e836d41
JustinSOrtega <justinsortega@gmail.com>
2019-07-29 11:09:42 -0500

    testing HL

---
+++ src/HighLow.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class HighLow {
+    public static void game() {
+        Scanner scanner = new Scanner(System.in);
+        int random = (int) (Math.random() * 99 + 1);
+        int guesses = 0;
+        boolean looper = false;
+        do {
+            System.out.println("Guess what number I'm thinking of: ");
+            guesses += 1;
+            int userGuess = scanner.nextInt();
+            if (userGuess == random) {
+                System.out.println("GOOD GUESS!");
+                System.out.println("Number of guesses: " + guesses);
+                looper = true;
+            } else if (userGuess < random) {
+                System.out.println("HIGHER!");
+            } else if (userGuess > random) {
+                System.out.println("LOWER!");
+            } else {
+                System.out.println("Something went wrong! Lets start over...");
+                game();
+            }
+        } while (looper);
+    }
+
+    public static void main(String[] args) {
+        game();
+    }
+
+
+}
