48e343844cd20f9941bf28a3fa56dd9b788f70a8
JustinSOrtega <justinsortega@gmail.com>
2019-07-29 13:46:44 -0500

    post lunch push

--- src/HighLow.java
+++ src/HighLow.java
@@ -7,10 +7,13 @@ public class HighLow {
         int guesses = 0;
         boolean looper = true;
         do {
-            System.out.println("Guess what number I'm thinking of: ");
+            System.out.println("Guess what number I'm thinking of: (you have 5 guesses)");
             guesses += 1;
             int userGuess = scanner.nextInt();
-            if (userGuess == random) {
+            if (guesses > 5) {
+                System.out.println("Sorry but you ran out of guesses, better luck next time!");
+                looper = true;
+            } else if (userGuess == random) {
                 System.out.println("GOOD GUESS!");
                 System.out.println("Number of guesses: " + guesses);
                 looper = false;
@@ -24,6 +27,7 @@ public class HighLow {
             }
         } while (looper);
     }
+
     public static void main(String[] args) {
         game();
     }
